6cbf7ad2f3b16c7b6a1b3f40cfdbae97aef72e40
anand gajjar <anand.gajjar@ineight.com>
2015-07-21 16:53:11 +0530

    add custom exception class

---
+++ src/com/lonelyplanet/exception/CustomException.java
@@ -0,0 +1,13 @@
+package com.lonelyplanet.exception;
+
+/**
+ *
+ * @author Anand Gajjar
+ */
+public class CustomException extends Exception {
+
+    //Constructor with message 
+    public CustomException(String s){  
+         super(s);  
+ }      
+}
