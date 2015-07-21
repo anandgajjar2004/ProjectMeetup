1b7ace53eb4c9e245360b91d38862c72efc0ff85
anand gajjar <anand.gajjar@ineight.com>
2015-07-21 23:03:56 +0530

    add newly created attributes in PDF format

--- src/com/lonelyplanet/export/details/PDF.java
+++ src/com/lonelyplanet/export/details/PDF.java
@@ -2,6 +2,7 @@ package com.lonelyplanet.export.details;
 
 import java.io.FileOutputStream;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
 import com.itextpdf.text.Paragraph;
@@ -13,24 +14,48 @@ import com.itextpdf.text.pdf.PdfWriter;
 import com.lonelyplanet.pojo.Alien;
 import com.lonelyplanet.exception.CustomException;
 import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
  
+/**
+ *
+ * @author Anand Gajjar
+ */
 public class PDF implements Format {
     
+    /**
+    *Implementation for PDF File using Itext library.
+    */    
     @Override
     public void generate(Alien alien) throws CustomException  {
     {
         try 
         {
+        //Create A4 size page with margin.  
         Document document = new Document(PageSize.A4, 10,10,10,10);
-        PdfWriter.getInstance(document, new FileOutputStream("sample.pdf"));            
+        PdfWriter.getInstance(document, new FileOutputStream("Permit_Form.pdf"));            
         document.open();
         
+        //Header Details.
+        Paragraph preface = new Paragraph("Permit Form. Welcome to Incredible Earth"); 
+        preface.setAlignment(Element.ALIGN_CENTER);
+        document.add(preface);
+        
+        DateFormat ddMMMyy = new SimpleDateFormat("dd/MMM/yyyy");
+        preface = new Paragraph(ddMMMyy.format(new Date())); 
+        preface.setAlignment(Element.ALIGN_CENTER);
+        document.add(preface);
+        
+        document.add(new Paragraph("\n"));
+        
+        //Create 1 column table to disaply image. 
         File file = new File("src/com/lonelyplanet/export/details/header.jpg");
 	String filepath = file.getAbsolutePath();						    	  		    	 	
                  
         Image image = Image.getInstance(filepath);            
         image.scaleToFit(1000f, 150f);        
-       
+               
         PdfPTable table = new PdfPTable(2);
         table.setWidthPercentage(100);
         
@@ -43,6 +68,7 @@ public class PDF implements Format {
         
         document.add(new Paragraph("\n"));
         
+        //Create 2 columns table to Display Alien's details. 
         table = new PdfPTable(2);
         table.setWidthPercentage(100);
         
@@ -65,6 +91,21 @@ public class PDF implements Format {
         cell = new PdfPCell(new Phrase("No. of Legs: "));        
         table.addCell(cell);
         cell = new PdfPCell(new Phrase(Integer.toString(alien.getNoOfLegs())));        
+        table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("No. of Nose: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(Integer.toString(alien.getNoOfNose())));        
+        table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("No. of Eyes: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(Integer.toString(alien.getNoOfEyes())));        
+        table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("No. of Days: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(Integer.toString(alien.getNoOfDays())));        
         table.addCell(cell);
         
         cell = new PdfPCell(new Phrase("Home Planet: "));        
