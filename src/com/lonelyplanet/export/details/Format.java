120d11d6a5cd4c81af278c1aacca135effa654b6
anand gajjar <anand.gajjar@ineight.com>
2015-07-21 16:15:53 +0530

    add comments and description for each method

--- src/com/lonelyplanet/export/details/Format.java
+++ src/com/lonelyplanet/export/details/Format.java
@@ -3,8 +3,9 @@ package com.lonelyplanet.export.details;
 import com.lonelyplanet.pojo.Alien;
 
 /**
- *
  * @author Anand Gajjar
+ * Required for Factory pattern. 
+ * Format interface for common behaviour across application.  
  */
 public interface Format {
     
