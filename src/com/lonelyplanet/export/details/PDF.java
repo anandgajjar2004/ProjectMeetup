e8724012d461811c926c7bf841b034e6be09af75
anand gajjar <anand.gajjar@ineight.com>
2015-07-21 09:29:51 +0530

    push new format of PDF

--- src/com/lonelyplanet/export/details/PDF.java
+++ src/com/lonelyplanet/export/details/PDF.java
@@ -4,6 +4,7 @@ import java.io.FileOutputStream;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -14,7 +15,13 @@ public class PDF implements Format {
     
     public static void main(String args[])
     {
-        new PDF().generate(null);
+        Alien a = new Alien();
+        a.setBloodColor("d");
+        a.setCodeName("d");
+        a.setHomePlanet("s");
+        a.setNoOfAntennas(3);
+        a.setNoOfLegs(3);
+        new PDF().generate(a);
     }
     
     @Override
@@ -22,27 +29,58 @@ public class PDF implements Format {
     {
         try 
         {
-        Document document = new Document(PageSize.A4, 0, 0, 0, 0);
+        Document document = new Document(PageSize.A4, 10,10,10,10);
         PdfWriter.getInstance(document, new FileOutputStream("sample.pdf"));            
         document.open();
         
          
         Image image = Image.getInstance("src/com/lonelyplanet/export/details/header.jpg");            
-        image.scaleToFit(1100f, 150f);
+        image.scaleToFit(1000f, 150f);
         
         
         PdfPTable table = new PdfPTable(2);
-        table.setWidthPercentage(90);
-        table.setSpacingBefore(5f);
-        table.setSpacingAfter(5f);
-        PdfPCell cell;
-        cell = new PdfPCell(image);        
-        cell.setRowspan(2);
+        table.setWidthPercentage(100);
+//        table.setSpacingBefore(5f);
+//        table.setSpacingAfter(5f);
+        
+        PdfPCell cell = new PdfPCell(image);        
+        cell.setColspan(2);
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));        
+        document.add(table);
+        
+        document.add(new Paragraph("\n"));
+        
+        table = new PdfPTable(2);
+        table.setWidthPercentage(100);
+//        table.setSpacingBefore(5f);
+//        table.setSpacingAfter(5f);
+        
+        cell = new PdfPCell(new Phrase("Code Name: "));        
         table.addCell(cell);
-        cell = new PdfPCell(new Phrase("Cell with rowspan 2"));        
+        cell = new PdfPCell(new Phrase(alien.getCodeName()));        
         table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("Blood Color: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(alien.getBloodColor()));        
+        table.addCell(cell);
+        
+        
+        cell = new PdfPCell(new Phrase("No. of Antennas: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(alien.getNoOfAntennas()));        
+        table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("No. of Legs: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(alien.getNoOfLegs()));        
+        table.addCell(cell);
+        
+        cell = new PdfPCell(new Phrase("Home Planet: "));        
+        table.addCell(cell);
+        cell = new PdfPCell(new Phrase(alien.getHomePlanet()));        
+        table.addCell(cell);
+        
         document.add(table);
         
         
